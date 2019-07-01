da17c5252a3bc5d7795457207d234433891be360
Dimama <citrus_m@mail.ru>
2019-07-01 19:30:19 +0300

    Add form for adding person

--- src/components/add-form/add-form.js
+++ src/components/add-form/add-form.js
@@ -1,13 +1,50 @@
 import React from 'react';
 
 
+import './add-form.css';
+
 export default class AddForm extends React.Component{
-    render() {
-        return (
-            <div>
-                <h2>Add form</h2>
-            </div>
-        )
-    }
+  state = {
+    name: '',
+    id: ''
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    const {name, id} = this.state;
+    console.log("Post to api");
+  };
+
+  onChangeName = (e) => {
+    this.setState({
+      name: e.target.value
+    })
+  };
+
+  onChangeId = (e) => {
+    this.setState({
+      id: e.target.value
+    })
+  };
+
+  render() {
+    return (
+      <form className="add-form d-flex" onSubmit={ this.onSubmit }>
+        <input className="mr-1"
+               type="text"
+               value={ this.state.name }
+               onChange={ this.onChangeName }
+               placeholder={ "Имя" }
+        />
+        <input className="mr-1"
+               type="number"
+               value={ this.state.id }
+               onChange={ this.onChangeId }
+               placeholder={ "Номер" }
+        />
+        <button type="submit" className="btn btn-dark"> Добавить </button>
+      </form>
+    )
+  }
 
 }
--- src/components/report-form/report-form.js
+++ src/components/report-form/report-form.js
@@ -2,12 +2,5 @@ import React from 'react';
 
 
 export default class ReportForm extends React.Component{
-    render() {
-        return (
-            <div>
-                <h2>Report form</h2>
-            </div>
-        )
-    }
 
 }
